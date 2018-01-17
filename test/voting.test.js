3f0a9de9596032fb20c15436f3132e463e0c5d78
Ng Guoyou <fauxface+github@gmail.com>
2018-01-17 12:15:15 +0800

    Initial Travis test commit

---
+++ test/voting.test.js
@@ -0,0 +1,11 @@
+const Voting = artifacts.require("Ballot");
+
+contract("Voting", accounts => {
+  it("should work", () => {
+    Voting.deployed()
+      .then(instance => instance.chairperson.call())
+      .then(addr => {
+        assert.equal(addr, accounts[0]);
+      });
+  });
+});
