6a1e6cf164dabda53df17eb281c2adfc08ca6004
Ng Guoyou <fauxface+github@gmail.com>
2018-01-17 14:41:06 +0800

    Dockerise tests

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ci: {
+      host: "ganache",
+      port: 8545,
+      network_id: "*" // Match any network id
     }
   }
 };
